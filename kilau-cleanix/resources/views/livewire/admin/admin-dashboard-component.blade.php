b884c6c1c01a7a770fd0a0c2a28ce24084c9830d
mevk2022 <99232261+mevk2022@users.noreply.github.com>
2022-11-18 17:52:25 +0800

    Dashboard Admin & Customer (Izzmir)

--- kilau-cleanix/resources/views/livewire/admin/admin-dashboard-component.blade.php
+++ kilau-cleanix/resources/views/livewire/admin/admin-dashboard-component.blade.php
@@ -1,5 +1,38 @@
 <div>
-<div><body style="background-color:#103C55;"></div>
-    <h1>Admin Dashboard</h1>
-    
+<div><body style="background-color:#77DD77;"></div>
+    <div class="container" style="padding: 30px 0">
+        <div class="row">
+            <div class="panel panel-default">
+                <div class="panel panel-heading">
+                    Admin Dashboard
+                </div>
+                <div class="panel-body">
+                    @if(Session::has('message'))
+                        <div class="alert alert-success" role="alert">{{Session::get('message')}}</div>
+                    @endif
+                    
+                        <div class="col-md-8">
+
+                            <div class="col-md-6">
+                                 <a href ="{{route('admin.service_categories')}}" class="btn btn-info pull-right">All Categories</a>
+                            </div>
+
+                            <div class="col-md-6">
+                                 <a href ="{{route('admin.all_services')}}" class="btn btn-info pull-right">All Services</a>
+                            </div>
+                            
+                            <div class="col-md-6">
+                                 <a href ="{{route('admin.contacts')}}" class="btn btn-info pull-right">All Contacts</a>
+                            </div>
+                            
+                            <div class="col-md-6">
+                                 <a href ="{{route('admin.booking_history')}}" class="btn btn-info pull-right">All Bookings</a>
+                            </div>
+
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
--- kilau-cleanix/resources/views/livewire/customer/customer-dashboard-component.blade.php
+++ kilau-cleanix/resources/views/livewire/customer/customer-dashboard-component.blade.php
@@ -1,4 +1,30 @@
 <div>
-<div><body style="background-color:#103C55;"></div>
-    <h1>Customer Dashboard</h1>
+<div><body style="background-color:#659DBD;"></div>
+    <div class="container" style="padding: 30px 0">
+        <div class="row">
+            <div class="panel panel-default">
+                <div class="panel panel-heading">
+                    Customer Dashboard
+                </div>
+                <div class="panel-body">
+                    @if(Session::has('message'))
+                        <div class="alert alert-success" role="alert">{{Session::get('message')}}</div>
+                    @endif
+                    
+                        <div class="col-md-8">
+
+                            <div class="col-md-6">
+                                 <a href ="{{route('user.profile')}}" class="btn btn-info pull-right">My Profile</a>
+                            </div>
+
+                            <div class="col-md-6">
+                                 <a href ="{{route('user.changepassword')}}" class="btn btn-info pull-right">Change Password</a>
+                            </div>
+                            
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
